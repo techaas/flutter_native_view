e7d60d906094d2d93b209bae0eaede00cc223a98
techaas <techaas@gmail.com>
2021-02-19 12:48:40 +0900

    add iOS native view

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,35 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
+    let viewFactory = FlutterNativeViewFactory()
+    registrar(forPlugin: "Embed")?.register(viewFactory, withId: "NativeView")
+
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+}
+
+public class FlutterNativeViewFactory: NSObject, FlutterPlatformViewFactory {
+    public func create(
+        withFrame frame: CGRect,
+        viewIdentifier viewId: Int64,
+        arguments args: Any?
+        ) -> FlutterPlatformView {
+        return FlutterNativeView(frame, viewId: viewId, args: args)
+    }
+}
+
+public class FlutterNativeView : NSObject, FlutterPlatformView {
+    let frame: CGRect
+    let viewId: Int64
+
+    init(_ frame: CGRect, viewId: Int64, args: Any?) {
+        self.frame = frame
+        self.viewId = viewId
+    }
+
+    public func view() -> UIView {
+        return UISlider(frame: frame)
+    }
 }
